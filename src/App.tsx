c270d64a78a85c58d526e5e9f65ec393b8fc624a
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-07-18 13:17:53 -0700

    change configs to use absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+  <>
+  <h1 className="text-3xl font-bold underline text-red-800">Hello world!</h1>
+  <p>hhhhhh</p>
+  </>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fem_result_component/"
-})
+  base: "/fem_result_component/",
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      assets: "/src/assets/",
+    },
+  },
+});
