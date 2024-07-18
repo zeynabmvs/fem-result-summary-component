cd5bc92d243cb4eb19251fd1643907222285bbb2
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-07-18 13:34:33 -0700

    add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ export default {
       fontFamily: {
         sans: ["Hanken", "ui-sans-serif", "system-ui"],
       },
+      backgroundImage: {
+        // Background
+        GradientLightSlateBlue: "hsl(252, 100%, 67%)",
+        GradientLightRoyalBlue: "hsl(241, 81%, 54%)",
+        // Circle
+        GradientVioletBlue: "hsla(256, 72%, 46%, 1)",
+        GradientPersianBlue: "hsla(241, 72%, 46%, 0)",
+      },
+      color: {
+        primary: {
+          red: "hsl(0, 100%, 67%)",
+          yellow: "hsl(39, 100%, 56%)",
+          green: "hsl(166, 100%, 37%)",
+          blue: " hsl(234, 85%, 45%)",
+        },
+        n: {
+          white: "hsl(0, 0%, 100%)",
+          "pale-blue": "hsl(221, 100%, 96%)",
+          lavendar: "hsl(241, 100%, 89%)",
+          "gray-blue": "hsl(224, 30%, 27%)",
+        },
+      },
     },
   },
   plugins: [],
