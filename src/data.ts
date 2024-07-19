266d7d062d7a982b4e33890e1965fcb2efa50d96
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-07-19 10:34:49 -0700

    move static files to public folder

--- src/data.ts
+++ src/data.ts
@@ -10,25 +10,25 @@ export const summary: SummaryItemProps[] = [
   {
     category: "Reaction",
     score: 80,
-    icon: "src/assets/images/icon-reaction.svg",
+    icon: "/images/icon-reaction.svg",
     variant: "red"
   },
   {
     category: "Memory",
     score: 92,
-    icon: "src/assets/images/icon-memory.svg",
+    icon: "/images/icon-memory.svg",
     variant: "yellow"
   },
   {
     category: "Verbal",
     score: 61,
-    icon: "src/assets/images/icon-verbal.svg",
+    icon: "/images/icon-verbal.svg",
     variant: "green"
   },
   {
     category: "Visual",
     score: 72,
-    icon: "src/assets/images/icon-visual.svg",
+    icon: "/images/icon-visual.svg",
     variant: "blue"
   },
 ];
