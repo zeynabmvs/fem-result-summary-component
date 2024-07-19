1c61cd769949668beeb858e88f7da897f3c4385f
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-07-19 10:26:28 -0700

    remove gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fem-result-summary-component/",
   // resolve: {
   //   alias: {
   //     src: "/src",
