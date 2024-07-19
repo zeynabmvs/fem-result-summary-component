68087105133529b7ed095846683d59eacc1ab7fc
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-07-18 18:01:00 -0700

    style fix

--- src/components/SummaryResult.tsx
+++ src/components/SummaryResult.tsx
@@ -3,15 +3,15 @@ import SummaryItem from "components/SummaryItem"
 
 const SummaryResult = () => {
     return (
-    <div className="grid grid-cols-2 w-[736px] h-[524px] bg-white shadow-2xl shadow-n-pale-blue rounded-[30px]">
+    <div className="grid grid-cols-2 w-[736px] h-[524px] bg-white shadow-[0_19px_50px_14px_rgba(0,0,0,0.25)] shadow-n-pale-blue rounded-[30px]">
       <div
         id="result"
         className="bg-gradient-blue flex flex-col gap-8 items-center  text-center justify-center text-white rounded-[30px]"
       >
         <h2 className="text-2xl opacity-50">Your Result</h2>
         <div className="flex flex-col gap-3 justify-center items-center size-[173px] rounded-full bg-gradient-circle">
-          <span className="text-[4rem] font-extrabold">76</span>
-          <span className="opacity-50">of 100</span>
+          <h3 className="text-6xl font-extrabold">76</h3>
+          <p className="opacity-50">of 100</p>
         </div>
         <h3 className="text-[2rem]">Great</h3>
         <p className="opacity-50 w-8/12">
