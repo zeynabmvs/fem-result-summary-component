10869911d9913bad7f5843046e337ef0a13259ab
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-07-20 09:18:12 -0700

    fix Accessibility

--- src/components/SummaryItem.tsx
+++ src/components/SummaryItem.tsx
@@ -15,7 +15,7 @@ const SummaryItem: React.FC<SummaryItemProps> = ({
     >
       <div className="flex gap-2">
         <img src={icon} alt={`${category} icon`} />
-        <h3 className={textColor}>{category}</h3>
+        <h2 className={textColor}>{category}</h2>
       </div>
       <span className="text-n-gray-blue">
         {score}
