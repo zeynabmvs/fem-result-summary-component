ad28c41e29915e5cb6fb9989a31f7197360fa1e0
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-07-20 13:36:48 -0700

    fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import SummaryResult from "@components/SummaryResult.tsx";
 
 function App() {
   return (
-    <div className="flex justify-center items-center md:h-screen">
       <SummaryResult />
-    </div>
   );
 }
 
